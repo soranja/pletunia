0aaaa0c6fd4f77c1b890be458aa1759cabadb9d3
Soranja <s.oranja.s@gmail.com>
2023-09-23 22:17:38 +0400

    hero mobile fixes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import HeroPostcard from "../../data/img/raw/postcards/iron-giant-hero.jpg";
-import Charm from "../../data/img/cut/bracelets_alpha/charm.png";
+import Lighthouse from "../../data/img/cut/lighthouse.png";
 import "./Hero.css";
 
 function Hero() {
   return (
     <section className="bg-hero bg-cover grow grid grid-cols-1 lg:grid-cols-hero mt-20 px-20 lg:pl-40 lg:pr-0 text-white relative">
-      <div className="lg:hidden relative -top-10 -right-5 rotate-7 h-40">
-        <img className="" src={Charm} alt="charm" />
+      <div className="lg:hidden mt-5">
+        <img className="" src={Lighthouse} alt="charm" />
       </div>
-      <div className="z-10 mb-20 gap-y-6 md:gap-y-10 lg:mb-0 flex flex-col items-start justify-end lg:justify-evenly">
-        <h2 className="font-serif text-xl text-center lg:text-left lg:leading-tight lg:text-6xl 2xl:text-8xl ">
+      <div className="z-10 mb-20 gap-y-6 md:gap-y-10 lg:mb-0 flex flex-col items-center lg:items-start justify-end lg:justify-evenly">
+        <h2 className="font-serif text-4xl text-center lg:text-left lg:leading-tight lg:text-6xl 2xl:text-8xl ">
           Bonds we <span className="italic">create</span> can hardly be broken
         </h2>
         <div className="hero-description text-center lg:text-left lg:text-2xl 2xl:w-7/12">
