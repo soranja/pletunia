ee0689c52af3ccaa702c0f04c58b409ac4e459d4
Soranja <s.oranja.s@gmail.com>
2023-09-23 23:42:23 +0400

    another fix on hero mobile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,13 +6,16 @@ import "./Hero.css";
 function Hero() {
   return (
     <section className="bg-hero bg-cover grow grid grid-cols-1 lg:grid-cols-hero mt-20 px-20 lg:pl-40 lg:pr-0 text-white relative">
-      <div className="lg:hidden mt-5">
-        <img className="" src={Lighthouse} alt="charm" />
-      </div>
-      <div className="z-10 mb-20 gap-y-6 md:gap-y-10 lg:mb-0 flex flex-col items-center lg:items-start justify-end lg:justify-evenly">
-        <h2 className="font-serif text-4xl text-center lg:text-left lg:leading-tight lg:text-6xl 2xl:text-8xl ">
-          Bonds we <span className="italic">create</span> can hardly be broken
-        </h2>
+      <div className="z-10 mb-20 mt-5 gap-y-6 md:gap-y-10 lg:mb-0 flex flex-col items-center lg:items-start justify-start lg:justify-evenly">
+        <div className="flex items-center">
+          <div className="lg:hidden mt-5 flex justify-center">
+            <img className="w-8/12" src={Lighthouse} alt="charm" />
+          </div>
+          <h2 className="font-serif text-2xl text-center mt-5 lg:text-left lg:leading-tight md:text-6xl 2xl:text-8xl ">
+            Bonds we <span className="italic">create</span> can hardly be broken
+          </h2>
+        </div>
+
         <div className="hero-description text-center lg:text-left lg:text-2xl 2xl:w-7/12">
           <p>
             Postcards, bracelets, and whatever keeps your friendship strong and
